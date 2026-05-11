cf75b3387802d6cfe6d892457791e629ff415f40
Owner <Owner@192.168.1.4>
2026-05-12 01:57:12 +0900

    自己紹介プログラム

--- HelloWorld_EisakuInoue/src/Mynameis/Main.java
+++ HelloWorld_EisakuInoue/src/Mynameis/Main.java
@@ -2,12 +2,14 @@ package Mynameis;
 
 class Main {
     public static void main(String[] args) {
-        Person person = new Person("田中太郎", 30, 1.75);
+        Person person = new Person("田中太郎", 30, 1.75, 60);
 
         System.out.println(person.name);
         System.out.println(person.age);
         System.out.println(person.height);
 
-        person.printData();
+        person.print();
+        
+        System.out.println("合計" + Person.count + "人です");
     }
 }
--- HelloWorld_EisakuInoue/src/Mynameis/Person.java
+++ HelloWorld_EisakuInoue/src/Mynameis/Person.java
@@ -4,16 +4,27 @@ public class Person {
 	String name;
 	int age;
 	double height;
+	double weight;
+    static int count = 0;
 	
 
-    Person(String name, int age, double height){
+    Person(String name, int age, double height,  double weight){
 		this.name = "井上英作";
 		this.age = 25;
 		this.height = 171;
+		this.weight = 75;
+		count++;
 	  }
-	void printData() {
+  
+
+	double bmi() {
+        return this.weight / (this.height * this.height);
+    }
+        
+	void print() {
         System.out.println("名前は" + name + "です");
         System.out.println("年齢は" + age + "歳です");
         System.out.println("身長は" + height + "mです");
+        System.out.println("BMIは" + this.bmi() + "です");
     }
 }
